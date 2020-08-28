3b731f8feb8b2ac0bab755443418d71cceb25f90
Boooob <emanuele.pisano@mail.polimi.it>
2020-08-28 22:47:48 +0200

    Optimized pearson computation Implemented multithreading

--- main.c
+++ main.c
@@ -13,7 +13,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "math.h"
+#include <math.h>
+#include <pthread.h>
 
 #define TRUE 0xff
 #define FALSE 0
@@ -33,20 +34,52 @@ typedef struct Subkey_element_s{
     struct Subkey_element_s* next;
 }Subkey_element;
 
+//float T[N][l*KEY_SIZE], char m[KEY_SIZE], Relation* relations[KEY_SIZE]
+typedef struct {
+    float** T;
+    char* m;
+    Relation** relations;
+}Thread_args;
+
 int l;        //number of points acquired per instruction processing
-float*** T;      //array containing all the power traces (divided per messages)
+//float T[M][N][l*KEY_SIZE];      //array containing all the power traces (divided per messages)
 int N;        //number of power traces captured from a device processing N encryptions of the same message
+//char m[M][KEY_SIZE];
 int M;        //number of plaintext messages
 float threshold;    //threshold for collision determination
 
-void get_relations(float*** T, int N, char** m, int M, Relation** relations);
+int max_threads;    //max number of threads to run simultaneously
+int threads_number = 0;
+pthread_mutex_t mutex_threads_number = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex_relations = PTHREAD_MUTEX_INITIALIZER;
+
+void get_relations(float T[M][N][l*KEY_SIZE], char m[M][KEY_SIZE], Relation** relations);
+void* find_collisions(void* args);
 void guess_key(Relation** relations);
-float standard_deviation(float** T, int theta, int t);
-float covariance(float** T, int theta0, int theta1, int t);
-void pearson(float** T, int theta0, int theta1, float* correlation);
+float standard_deviation(float T[N][l*KEY_SIZE], int theta, int t);
+//float covariance(float T[N][l*KEY_SIZE], int theta0, int theta1, int t);
+float covariance(float T[N][l*KEY_SIZE], int theta0, int theta1, int t, float* sum_array);
+void pearson(float T[N][l*KEY_SIZE], int theta0, int theta1, float* correlation);
+float optimized_pearson(float T[N][l*KEY_SIZE], int theta0, int theta1, float* sum_array, float* std_dev_array);
 Subkey_element* guess_subkey(int to_guess, Relation** relations, int* guessed);
 void resolve_relations(int start, Relation** relations, char* new_guessed, char* xor_array);
 void combine_subkeys(Subkey_element* subkeys_list, int* partial_key);
+
+void create_params(float T[M][N][l*KEY_SIZE], char m[M][KEY_SIZE]){
+    int i,j,k, h;
+    for(i=0; i<M; i++){
+        for(j=0; j<N; j++){
+            for(k=0; k<KEY_SIZE; k++){
+                for(h=0; h<l; h++){
+                    T[i][j][k*l+h] = h;
+                }
+            }
+        }
+        for(j=0; j<KEY_SIZE; j++){
+            m[i][j] = (char) j;
+        }
+    }
+}
 
 int main(int argc, char** argv) {
     /*
@@ -55,19 +88,47 @@ int main(int argc, char** argv) {
     //power traces are provided as array of integers (each int is the measured power in the correspondant instant)
     T = argv[2];    
     N = sizeof(T[0])/sizeof(T[0][0]);     //controllare se funziona
-    char** m = argv[3];     //array containing all the plaintext messages
+    char m[M][KEY_SIZE] = argv[3];     //array containing all the plaintext messages
     M = sizeof(m)/sizeof(m[0]);
     //TODO controllare se l divide N?
     threshold = argv[4];
     */
-    char** m;
+    
+    
+    l = 3;
+    N = 5;
+    M = 3;
+    threshold = 0.9;
+    max_threads = 4;
+    float T[M][N][l*KEY_SIZE];
+    char m[M][KEY_SIZE];
+    create_params(T, m);
     
     Relation* relations[KEY_SIZE];
     
-    get_relations(T, N, m, M, relations);
+    get_relations(T, m, relations);
     guess_key(relations);
     
-    //liberare memoria alla fine
+    //free memory allocated for relations lists
+    Relation* pointer_relations_current;
+    Relation* pointer_relations_next;
+    for(int i=0; i<KEY_SIZE; i++){
+        pointer_relations_current = relations[i];
+        if(pointer_relations_current!=NULL){
+            pointer_relations_next = pointer_relations_current->next;
+            while(pointer_relations_next!=NULL){
+                free(pointer_relations_current);
+                pointer_relations_current = pointer_relations_next;
+                pointer_relations_next = pointer_relations_current->next;
+            }
+            free(pointer_relations_current);
+        }
+    }
+    
+    //free mutexes
+    pthread_mutex_destroy(&mutex_relations);
+    pthread_mutex_destroy(&mutex_threads_number);
+    
     return (EXIT_SUCCESS);
 }
 
@@ -75,37 +136,59 @@ int main(int argc, char** argv) {
 //compare the value of a synthetic criterion with a practically determined threshold
 //as criterion we used the maximum Pearson correlation factor
 //theta0 and theta1 are time instant at which instruction starts (for performance reasons)
-int collision(float** T, int theta0, int theta1){
+int collision(float T[N][l*KEY_SIZE], int theta0, int theta1, float* sum_array, float* std_dev_array){
     float correlation[l];
-    pearson(T, theta0, theta1, correlation);
-    float max = correlation[0];
-    for(int i=1; i<l; i++){
-        if(correlation[i]>max){
-            max = correlation[i];
-        }
-    }
-    if(max>threshold){
+    //pearson(T, theta0, theta1, correlation);
+    float max_correlation = optimized_pearson(T, theta0, theta1, sum_array, std_dev_array);
+    
+    //check if max is greater than threshold
+    if(max_correlation>threshold){
         return TRUE;
     }
     else{
         return FALSE;
     }
-    //TODO migliorabile anche questa, tipo calcolando il max direttamente in pearson, appena dopo averlo calcolato
 }
 
-void pearson(float** T, int theta0, int theta1, float* correlation){
+void pearson(float T[N][l*KEY_SIZE], int theta0, int theta1, float* correlation){
     int i;
     for(i=0; i<l; i++){
-        correlation[i] = covariance(T, theta0, theta1, i)/(standard_deviation(T, theta0, i)*standard_deviation(T, theta1, i));
+//        correlation[i] = covariance(T, theta0, theta1, i)/(standard_deviation(T, theta0, i)*standard_deviation(T, theta1, i));
     }
 }
 
-float* optimized_pearson(){
-    //TODO
-    //per ottimizzare forse si potrebbe calcolare insieme covariance e standard_deviation (per accedere meglio agli array, da verificare con qualche prova)
+//efficiently compute in one step both sum and std deviation
+void compute_arrays(float T[N][l*KEY_SIZE], float* sum_array, float* std_dev_array){
+    float sum;
+    float squared_sum;
+    for(int i=0; i<(l*KEY_SIZE); i++){
+        sum = 0;
+        squared_sum = 0;
+        for(int j=0; j<N; j++){
+            sum += T[j][i];
+            squared_sum += T[j][i]*T[j][i];
+        }
+        sum_array[i] = sum;
+        std_dev_array[i] = sqrtf((N*squared_sum)-(sum*sum));
+    }
 }
 
-float covariance(float** T, int theta0, int theta1, int t){
+float optimized_pearson(float T[N][l*KEY_SIZE], int theta0, int theta1, float* sum_array, float* std_dev_array){
+    int i;
+    float correlation;
+    float max_correlation = 0;
+    for(i=0; i<l; i++){
+        //correlation[i] = covariance(T, theta0, theta1, i)/(standard_deviation(T, theta0, i)/standard_deviation(T, theta1, i));
+        correlation = covariance(T, theta0, theta1, i, sum_array)  /  (std_dev_array[theta0+i] * std_dev_array[theta1+i]);
+        if(correlation > max_correlation){
+            max_correlation = correlation;
+        }
+    }
+    return max_correlation;
+}
+
+/*
+float covariance(float T[N][l*KEY_SIZE], int theta0, int theta1, int t){
     int i;
     float first_sum=0, second_sum=0, third_sum=0;
     for(i=0; i<N; i++){
@@ -115,8 +198,19 @@ float covariance(float** T, int theta0, int theta1, int t){
     }
     return (N*first_sum)-(second_sum*third_sum);
 }
+ * */
 
-float standard_deviation(float** T, int theta, int t){
+//for optimized pearson
+float covariance(float T[N][l*KEY_SIZE], int theta0, int theta1, int t, float* sum_array){
+    int i;
+    float first_sum=0;
+    for(i=0; i<N; i++){
+        first_sum += (T[i][theta0+t]*T[i][theta1+t]);
+    }
+    return (N*first_sum)-(sum_array[theta0+t]*sum_array[theta1+t]);
+}
+
+float standard_deviation(float T[N][l*KEY_SIZE], int theta, int t){
     int i;
     float first_sum=0, second_sum=0;
     for(i=0; i<N; i++){
@@ -126,38 +220,76 @@ float standard_deviation(float** T, int theta, int t){
     return sqrtf((N*first_sum)-(second_sum*second_sum));
 }
 
-void get_relations(float*** T, int N, char** m, int M, Relation** relations){
+void get_relations(float T[M][N][l*KEY_SIZE], char m[M][KEY_SIZE], Relation* relations[KEY_SIZE]){
     int i, j, k;
+    pthread_t thread_id;
+    float sum_array[KEY_SIZE];
+    float standard_deviation_array[KEY_SIZE];
+    
     for(i=0; i<KEY_SIZE; i++){
         relations[i]=NULL;
     }
     for(i=0; i<M; i++){
-        for(j=0; j<KEY_SIZE; j++){//TODO capire se è giusto, magari il calcolo degli istanti di tempo è più difficile
-            for(k=j+1; k<KEY_SIZE; k++){//TODO in caso modificare anche qua
-                if(collision(T[i], j*l, k*l)){
-                    /* if a collision is detected, two new relations are created:
-                     * from byte j to k and viceversa. This to achieve better 
-                     * performance when searching for a relation involving a 
-                     * specific byte
-                     */
-                    
-                    //controllare che la relazione non esissta già
-                    Relation* new_relation = malloc(sizeof(Relation));
-                    char new_value = (m[i][j])^(m[i][k]);
-                    new_relation->in_relation_with = k;
-                    new_relation->value = new_value;
-                    new_relation->next = relations[j];
-                    relations[j] = new_relation;
-                    
-                    new_relation = malloc(sizeof(Relation));
-                    new_relation->in_relation_with = j;
-                    new_relation->value = new_value;
-                    new_relation->next = relations[k];
-                    relations[k] = new_relation;
-                }
-            }      
+        while(threads_number >= max_threads){
+            sleep(500);
         }
+        pthread_mutex_lock(&mutex_threads_number); 
+        threads_number++;
+        pthread_mutex_unlock(&mutex_threads_number); 
+        Thread_args* args = malloc(sizeof(Thread_args));
+        args->T = T[i];
+        args->m = m[i];
+        args->relations = relations;
+        pthread_create(&thread_id, NULL, find_collisions, (void* ) args);
     }
+    while(threads_number > 0){
+        sleep(500);
+    }
+}
+
+//check collisions for a given power trace 
+void* find_collisions(void* args){
+    Thread_args* args_casted = (Thread_args*) args;
+    float** T = args_casted->T;
+    char* m = args_casted->m;
+    Relation** relations = args_casted->relations;
+    float sum_array[KEY_SIZE*l];
+    float std_dev_array[KEY_SIZE*l];
+    compute_arrays(T, sum_array, std_dev_array);
+    for(int j=0; j<KEY_SIZE; j++){//TODO capire se è giusto, magari il calcolo degli istanti di tempo è più difficile
+        for(int k=j+1; k<KEY_SIZE; k++){//TODO in caso modificare anche qua
+            if(collision(T, j*l, k*l, sum_array, std_dev_array)){
+                /* if a collision is detected, two new relations are created:
+                 * from byte j to k and viceversa. This to achieve better 
+                 * performance when searching for a relation involving a 
+                 * specific byte
+                 */
+
+                //controllare che la relazione non esissta già
+                //usare mutex!
+                pthread_mutex_lock(&mutex_relations); 
+                
+                Relation* new_relation = malloc(sizeof(Relation));
+                char new_value = (m[j])^(m[k]);
+                new_relation->in_relation_with = k;
+                new_relation->value = new_value;
+                new_relation->next = relations[j];
+                relations[j] = new_relation;
+
+                new_relation = malloc(sizeof(Relation));
+                new_relation->in_relation_with = j;
+                new_relation->value = new_value;
+                new_relation->next = relations[k];
+                relations[k] = new_relation;
+                
+                pthread_mutex_unlock(&mutex_relations); 
+            }
+        }      
+    }
+    pthread_mutex_lock(&mutex_threads_number); 
+    threads_number--;
+    pthread_mutex_unlock(&mutex_threads_number);
+    pthread_exit(NULL);
 }
 
 
@@ -187,7 +319,16 @@ void guess_key(Relation** relations){
         initial_key[i] = 0;
     }
     combine_subkeys(subkeys_list, initial_key);
-    //liberare memoria subkeys
+    
+    //free memory allocated for subkeys
+    Subkey_element* subkey_pointer_current = subkeys_list;
+    Subkey_element* subkey_pointer_next = subkeys_list->next;
+    while(subkey_pointer_next!=NULL){
+        free(subkey_pointer_current);
+        subkey_pointer_current = subkey_pointer_next;
+        subkey_pointer_next = subkey_pointer_current->next;
+    }
+    free(subkey_pointer_current);
 }
 
 Subkey_element* guess_subkey(int to_guess, Relation** relations, int* guessed){
@@ -206,12 +347,12 @@ Subkey_element* guess_subkey(int to_guess, Relation** relations, int* guessed){
     if(relations[to_guess]!=NULL){
         resolve_relations(to_guess, relations, new_guessed, xor_array);
     }
-    for(c=0; c<256; c++){
+    for(c=0; c<256; c++){   //sostituire 256 con byte_space?
         for(i=0; i<KEY_SIZE; i++){
             random_value[i]=c;
         }
         for(i=0; i<KEY_SIZE_INT; i++){
-            ((int**) (new_subkeys->subkeys))[c][i] = ((int*) random_value)[i] ^ ((int*) xor_array)[i] & ((int*) new_guessed)[i];     //int optimization
+            ((int*) ((new_subkeys->subkeys)[c]))[i] = (((int*) random_value)[i] ^ ((int*) xor_array)[i]) & ((int*) new_guessed)[i];     //int optimization
         }
     }
     for(i=0; i<KEY_SIZE; i++){
