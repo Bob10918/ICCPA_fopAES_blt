e165edc616e4749b6b153c30b9e2174795b69993
Boooob <emanuele.pisano@mail.polimi.it>
2020-10-24 17:25:33 +0200

    Correctly implemented multithreading

---
+++ calculate_collisions_double.c
@@ -0,0 +1,14 @@
+#include "iccpa.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <math.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+
+void calculate_collisions_double(FILE* infile, int max_threads_number, Relation** relations){};
