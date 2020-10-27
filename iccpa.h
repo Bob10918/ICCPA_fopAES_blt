4dba78dbeccbb23dd2acc53e312e431d3cbcb5ee
Boooob <emanuele.pisano@mail.polimi.it>
2020-10-27 15:08:53 +0100

    Final implementation working, various bug fixes

--- iccpa.h
+++ iccpa.h
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
  * File:   iccpa.h
- * Author: bob
+ * Author: Emanuele Pisano
  *
- * Created on 24 ottobre 2020, 12.21
  */
 
 #ifndef ICCPA_H
